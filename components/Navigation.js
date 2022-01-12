acf11dd1ae39e319955640f1acfc4e4244c7f7dd
Yente <Yente.Janssens@ugent.be>
2022-01-12 15:58:57 +0100

    Logo YJ

--- components/Navigation.js
+++ components/Navigation.js
@@ -43,7 +43,7 @@ const Navigation = ({ locale, locales }) => {
               alt="IMDBPlus Logo"
               className=""
             /> */}
-            <Image src="/Travel.png" alt="IMDBPlus Logo" width="90" height="25" />
+            <Image src="/Travel.png" alt="IMDBPlus Logo" width="70" height="25" />
           </a>
         </div>
         <div className={styles.navlinkswrapper}>
