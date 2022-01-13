71399da2facd06be51b290d1218171dea57a546a
Yente <Yente.Janssens@ugent.be>
2022-01-13 21:22:30 +0100

    Newsletter.Fix.YJ

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-{ formstate&&<iframe width="540" height="305" src="https://8fc47476.sibforms.com/serve/MUIEAHBHnOLZ5rfcpxFqTeTA_JervZkd8LiU4Acm9l81slR030o_rqWNlz51j4wQpZ7F2RlPZSgx63djMLIp0e4GQVlI5hhTdQ3vejD5x2_NcGgvNT0B1L9NKBTuaCf_Zv3hsS0Sg5Z6cYZeZX98xL_mTZJJcQoEnlLSPuLGXHaA48xqyxle2PuazBSC6QwQ5wYpbeDvHmhAVrlA" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>}
+{ formstate&&<iframe width="540" height="900" src="https://8fc47476.sibforms.com/serve/MUIEAERzQOBVxyFnHjg4ycj-hGqPkFI-KNo8B7SCdyQmZFDl21D54EOF1PJjXggfKzH5wAmq5B9xz2GkY2fowYpFfRJAKFiSr4uVk0nTLMyZTSJXkNh472W3FdzO3EvCiGyUZ8ZmRoz_NXkdI4FkbqPzlReJsoQg6J0jqP0ptM-vCZk-jI6h2yEZ6odHL3nJtJMKKeOEfV_EETFA" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>}
     </div>
   );
 };
